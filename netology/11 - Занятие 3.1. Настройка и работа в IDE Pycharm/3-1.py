232e402991c90efd37a66ae8813c9a1adb041a2f
desve <2901243@mail.ru>
2017-01-06 07:35:31 +0000

    Занятие 3.1. Настройка и работа в IDE Pycharm

---
+++ netology/11 - Занятие 3.1. Настройка и работа в IDE Pycharm/3-1.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+# Занятие 3.1. Настройка и работа в IDE Pycharm
+
+import glob
+import os.path
+import codecs
+
+# Определяем где мы находимся
+PATH = os.getcwd()
+
+print('Из какой папки будем парсить файлы?')
+print('1 - Migrations 2 - Advanced Migrations')
+
+while True:
+    var = input()
+    if var == '1':
+        # Переходим в папку Migrations
+        os.chdir(PATH + '/Migrations')
+        break
+    elif var == '2':
+        os.chdir(PATH + '/Advanced Migrations')
+        break
+    else:
+        print('необходимо ввести 1 или 2')
+
+# Сохраняем имена всех .sql файлов
+sql_files = glob.glob('*.sql')
+print('Всего найдено %s .sql файлов' % len(sql_files))
+
+files = []
+while True:
+    print('Рассматриваем %s файллов' % len(sql_files))
+    print('введите запрос для парсинга')
+    user_input = str(input())
+    # INSERT
+    # APPLICATION_SETUP
+    # A400M
+    # 0.0
+    # 2.0
+    for file in sql_files:
+        print('Рассматриваем файл', file)
+        try:
+            file_encoding = 'utf-8'
+            with codecs.open(file, encoding=file_encoding) as new_file:
+                info = new_file.read()
+        except UnicodeError:
+            file_encoding = 'koi8-r'
+            with codecs.open(file, encoding=file_encoding) as new_file:
+                info = new_file.read()
+            if info.find(user_input) > 0:
+                files.append(file)
+                print('В файле %s подстрока %s найдена' % (file, user_input))
+            else:
+                print('В файле %s подстрока %s не найдена' % (file, user_input))
+    print('В %s файлах содержится подстрока %s' % (len(files), user_input))
+
+    files, sql_files = [], files
+
