d5ec086ca20f893d2e51bf694aad4e957665d2a9
desve <2901243@mail.ru>
2017-01-10 18:04:18 +0000

    Занятие 2.4. Вызов внешних программ. Исправление

---
+++ netology/10/10-2.py
@@ -0,0 +1,49 @@
+# sudo apt-get update
+# sudo apt-get install imagemagick
+
+import subprocess
+import os
+import os.path
+import glob
+from multiprocessing import Pool
+
+# Определяем наше местоположение на диске
+global PATH
+PATH = os.getcwd()
+
+# Определяем откуда брать файлы
+file_path_input = os.path.join(PATH, 'Source')
+
+# Переходим к папке с исходными фото 
+os.chdir(file_path_input)
+
+# Создаем список, имеющихся там файлов
+files = glob.glob('*.jpg')
+
+# Создаем деректорию для обработанных фото
+try:
+    os.mkdir(os.path.join(PATH, 'Result'))
+except FileExistsError:
+    pass
+
+def file_convert(file):
+    global PATH
+# Извлекаем файлы и обрабатываем их
+    file_path_output = os.path.join(PATH, 'Result', file)
+    command = 'convert ' + file + ' -resize 200 ' + file_path_output
+    go = subprocess.call(command, shell = True)
+
+""" 
+# Обычный вызов подпрограммы
+for file in files:
+    file_convert(file)
+""" 
+ 
+# Попытка организации параллейных процессов
+if __name__ == '__main__':
+    with Pool(4) as p:
+        p.map(file_convert, files)
+        
+print('Задание выполнено')
+ 
+
